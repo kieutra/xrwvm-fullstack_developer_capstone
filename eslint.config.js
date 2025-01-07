9d81020cac4f6aa0870d7d6fbc265658db74c2c2
kieutra <kieutra@gmail.com>
2025-01-07 17:49:47 -0500

    add esling.config.js

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: [
+      "eslint:recommended", // You can add more configurations, e.g., airbnb, prettier
+    ],
+    rules: {
+      // Customize rules here
+    },
+  };
+  
