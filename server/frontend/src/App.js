94ef1bd368e8d6b3a6267ff731ef8658db9ae16b
kieutra <kieutra@gmail.com>
2025-01-07 15:28:30 -0500

    install jshint 1

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,4 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
@@ -16,4 +16,5 @@ function App() {
     </Routes>
   );
 }
+
 export default App;
